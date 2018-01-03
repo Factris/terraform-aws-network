d80b7a602c7a083ec7b21f49b25a24c2d6d23daf
Jasper Laagland <jasper@jungo.nl>
2018-01-03 15:31:14 +0100

    userdata file reference

--- userdata.tf
+++ userdata.tf
@@ -1,6 +1,6 @@
 data "template_file" "user-data" {
 
-  template = "${file(var.userdata_path)}"
+  template = "${file(${path.module}/var.userdata_path)}"
 
   vars {
     authorized_keys   =  "  - ${join("\n  - ", split("\n", trimspace(file(var.authorized_keys))))}"
